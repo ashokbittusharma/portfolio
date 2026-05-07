4824572460545c2a88887d5bd16e229ba6607021
Ashok Sharma <ashokbittusharma@hotmail.com>
2026-05-07 21:38:49 +0530

    inital setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header, Topics, Info, Testimonials, Footer} from './components';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Topics />
+    <Info />
+    <Testimonials />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Info from "./Info/Info";
+import Testimonials from "./Testimonials/Testimonials";
+import Topics from "./Topics/Topics";
+import Footer from "./Footer/Footer";
+
+export {
+    Header,
+    Info,
+    Topics,
+    Testimonials,
+    Footer
+}
