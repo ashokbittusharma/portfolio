21c9a4ee4110410a731dfd58014b4b2d24ac5090
Ashok Sharma <ashokbittusharma@hotmail.com>
2026-05-11 21:52:32 +0530

    Header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,16 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="wrapper">
+        <div className="cta">
+          <p className='course-name'>Sound Design MasterClass</p>
+        <h1>Learn the art of sound design</h1>
+        <a href='#'>Demo Lesson</a>
+        </div>
+        
+      </div>
+    </header>
   )
 }
 
